0c8a80e72ce2b4ba5670ccceecfec738293fad57
Настя <nglazovskaa@gmail.com>
2023-11-19 16:42:05 +0300

    update

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
 import numpy as np
 Base = declarative_base()
-class Storyes(Base):
-    __tablename__ = 'Storyes'
+class Storyes1(Base):
+    __tablename__ = 'Storyes1'
     __table_args__ = (
-        UniqueConstraint('storyid', name='unique_id'),
+        UniqueConstraint('storyid1', name='unique_id1'),
     )
 
-    storyid = Column(Integer, primary_key=True, autoincrement=True)
+    storyid1 = Column(Integer, primary_key=True, autoincrement=True)
     storyText = Column(String, nullable=False)
     storyVector = Column(String, nullable=False)
 
@@ -18,6 +18,8 @@ if __name__ == '__main__':
 
  postgresql_url = 'postgresql://postgres:12345@localhost:5432/psycho'
  engine = create_engine(postgresql_url)
-
+ Session = sessionmaker(bind=engine)
+ session = Session()
  # Создаем таблицу
  Base.metadata.create_all(engine)
+ session.commit()
